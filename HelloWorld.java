ad9f0d837138a06104de00abdb2505b714f82fe9
henrylwang <henrylwang@tencent.com>
2019-07-04 11:46:47 +0800

    com:

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+/*
+ * Author: wangpeng
+ * Created Time: Thu Jul  4 11:45:41 2019
+ */
+public class Main {
+  public static void main(String argv[]) {
+    System.out.println("Hello for Jenkins...");
+  }
+}
